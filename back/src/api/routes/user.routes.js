373404ed50214509860681bb69c4561a80b8ab80
Wida30 <wida30@yahoo.es>
2022-07-07 20:41:08 +0200

    auth

--- back/src/api/routes/user.routes.js
+++ back/src/api/routes/user.routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login, getAllUsuarios, patchUsuarios, getUsuarioID, deleteUsuario } = require("../controllers/user.controller");
+const {  isRegistered, isAdmin } = require("../../middlewares/auth.middleware");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users", getAllUsuarios);
 router.patch("/:id", patchUsuarios);
 router.get("/:id", getUsuarioID);
-router.delete("/:id", deleteUsuario)
+router.delete("/:id",  deleteUsuario)
 
 module.exports = router;
--- back/src/middlewares/auth.middleware.js
+++ back/src/middlewares/auth.middleware.js
@@ -1,45 +1,86 @@
-
 const jwt = require("jsonwebtoken");
 const HTTPSTATUSCODE = require("../utils/httpStatusCode");
+const bcrypt = require("bcrypt");
+const {JwtUtil} = require ("../utils/jwt");
 
-const isAuth = (req, res, next) => {
-  
-  const authorization = req.headers.authorization;
-  
-  if (!authorization) {
-    return res.json({
-      status: 401,
-      message: HTTPSTATUSCODE[401],
-      data: null,
-    });
-  }
-  
-  const splits = authorization.split(" ");
-  if (splits.length != 2 || splits[0] != "Bearer") {
-    return res.json({
-      status: 400,
-      message: HTTPSTATUSCODE[400],
-      data: null,
-    });
-  }
-  
-  const jwtString = splits[1];
 
+
+const isRegistered = async (req, res, next) => {
   try {
-      
-      var token = jwt.verify(jwtString, req.app.get("secretKey"))
+    const token = req.headers.authorization;
+    if (!token) {
+      return next(setError(404, "Unauthorized"));
+    }
+    const parsedToken = token.replace("Bearer ", "");
+    const validToken = JwtUtil(parsedToken, process.env.JWT_SECRET);
+    const userLogued = await User.findById(validToken.id);
+
+    userLogued.password = null;
+    req.user = userLogued;
+    next();
   } catch (error) {
-      return next(error);
+    return next(error);
   }
-  
-  const authority = {
-      id: token.id,
-      name: token.name
-  }
- 
-  req.authority = authority;
-  
-  next()
 };
 
-module.exports = {isAuth}
+const isAdmin = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return next(setError(404, "Unauthorized"));
+    }
+    const parsedToken = token.replace("Bearer ", "");
+    const validToken = JwtUtil(parsedToken, process.env.JWT_SECRET);
+    const userLogued = await User.findById(validToken.id);
+
+    if (userLogued.rol === "admin") {
+      userLogued.password = null;
+      req.user = userLogued;
+      next();
+    } else {
+      return next("no eres admin");
+    }
+  } catch (error) {
+    return next("error");
+  }
+};
+
+module.exports = {  isRegistered, isAdmin };
+
+// const isAuth = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+
+//   if (!authorization) {
+//     return res.json({
+//       status: 401,
+//       message: HTTPSTATUSCODE[401],
+//       data: null,
+//     });
+//   }
+
+//   const splits = authorization.split(" ");
+//   if (splits.length != 2 || splits[0] != "Bearer") {
+//     return res.json({
+//       status: 400,
+//       message: HTTPSTATUSCODE[400],
+//       data: null,
+//     });
+//   }
+
+//   const jwtString = splits[1];
+
+//   try {
+//     var token = jwt.verify(jwtString, req.app.get("secretKey"));
+//   } catch (error) {
+//     return next(error);
+//   }
+
+//   const authority = {
+//     id: token.id,
+//     name: token.name,
+//   };
+
+//   req.authority = authority;
+
+//   next();
+// };
