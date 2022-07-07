7d4bef0b39bcd64c08ed08e819a0af0413a6c475
Francisco <frandeluque@AirdeFrancisco.technicolor.net>
2022-07-07 11:16:18 +0200

    feat cromo-routes

---
+++ back/src/api/controllers/cromos.controllers.js
@@ -0,0 +1,107 @@
+const { deleteFile } = require("../../middlewares/deleteFile");
+const Cromo = require("../models/cromos.models");
+const HTTPSTATUSCODE = require("../../utils/httpStatusCode")
+
+
+
+const getAllCromos = async (req, res, next) => {
+  try {
+    const allCromos = await Cromo.find();
+    return res.json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      Cromo: allCromos,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const getCromoByID = async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const cromoByID = await Cromo.findById(id);
+      return res.json({
+        status: 200,
+        message: HTTPSTATUSCODE[200],
+        Cromo: cromoByID,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  const getCromoByNombre = async (req, res, next) => {
+    try {
+      const {nombre} = req.params;
+
+      const cromoByNombre = await Cromo.findOne({nombre : nombre});
+      return res.json({
+        status: 200,
+        message: HTTPSTATUSCODE[200],
+        cromo: cromoByNombre,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  const createCromos = async (req, res, next) => {
+    try {
+      const newCromos = new Cromo(req.body);
+      if (req.file) {
+        newCromos.imagen = req.file.path;
+      }
+      const createdCromos = await newCromos.save();
+      return res.json({
+        status: 201,
+        message: HTTPSTATUSCODE[201],
+        cromo: createdCromos,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  const deleteCromos = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      const cromoBorrado = await Cromo.findByIdAndDelete(id);
+  
+      return res.status(200).json(cromoBorrado);
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  const patchCromos = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      const patchCromo = new Cromo(req.body);
+  
+      patchCromo._id = id;
+
+      const cromoData= await Cromo.findByIdAndUpdate(id,patchCromo)
+
+    //   patchMesa.autor =[...mesaData.autor, ...patchMesa.autor]
+
+      if (cromoData.imagen) {
+        deleteFile(cromoData.imagen);
+        }
+
+      if (req.file) {
+        patchCromo.imagen = req.file.path;
+      }
+  
+    //   const CromoDB = await Mesa.findByIdAndUpdate(id, patchMesa);
+      
+      return res.status(200).json({ nuevo: patchCromo, vieja: cromoData });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+
+  module.exports = {getAllCromos, getCromoByID, getCromoByNombre, createCromos, deleteCromos, patchCromos}
---
+++ back/src/api/routes/cromos.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+const upload = require("../../middlewares/file")
+// const {isAuth} = require("../../middlewares/auth.middleware");
+
+const  {getAllCromos, getCromoByID, getCromoByNombre, createCromos, deleteCromos, patchCromos} = require("../controllers/cromos.controllers");
+
+
+router.get("/", getAllCromos);
+router.get("/id/:id", getCromoByID);
+router.get("/nombre/:nombre", getCromoByNombre);
+router.post("/", upload.single("imagen"), createCromos);
+router.delete('/:id', upload.single("imagen"), deleteCromos);
+router.patch('/:id', upload.single("imagen"), patchCromos)
+
+module.exports = router;
