b30fab437072600f9d0cf4e4bf0917afd5f65458
Wida30 <wida30@yahoo.es>
2022-07-07 18:03:08 +0200

    administrador

--- front/src/pages/Register/Register.jsx
+++ front/src/pages/Register/Register.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import RegisterComponent from '../../components/RegisterComponent/RegisterComponent'
+
 const Register = () => {
   return (
-    <div>Register</div>
+   <RegisterComponent/>
   )
 }
 
