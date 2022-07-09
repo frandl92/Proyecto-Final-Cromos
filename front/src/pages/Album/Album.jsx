4d6e142f4257049921e8bde7da5a3d72054aa221
Francisco <frandeluque@AirdeFrancisco.technicolor.net>
2022-07-09 20:01:39 +0200

    feat setterCromo

--- front/src/pages/Album/Album.jsx
+++ front/src/pages/Album/Album.jsx
@@ -1,33 +1,23 @@
-import React, { useContext } from "react";
-import { SWContext } from "../../context/context";
-import "./album.scss"
+import React, { useContext, useState } from 'react';
+import { SWContext } from '../../context/context';
+import './album.scss';
 
 const Album = () => {
   const { cromos } = useContext(SWContext);
+  const [getCromo, setGetCromo] = useState();
 
   return (
     <>
-
- 
       <h1>esto es pagina album no tiene nada componetizado</h1>
-
-    
+      <img className='nav' src={getCromo} alt='...'></img>
       {cromos.length ? (
-        <div className="album">
+        <div className='album'>
           {cromos.map((cromo) => (
-            <figure key={cromo._id} className="cromo">
-
-              
-
+            <figure key={cromo._id} className='cromo' onClick={()=> setGetCromo (cromo.imagen)} >
               <p>{cromo.posicion}</p>
               <img src={cromo.imagen} alt={cromo.nombre} />
 
-              
-
-
               <h3>{cromo.nombre}</h3>
-            
-
             </figure>
           ))}
         </div>
