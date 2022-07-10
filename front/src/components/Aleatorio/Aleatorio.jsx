a8e75bdb8ea150c0730c77d110627a960bb11943
Francisco <frandeluque@AirdeFrancisco.technicolor.net>
2022-07-10 13:41:14 +0200

    feat fran2

--- front/src/components/Aleatorio/Aleatorio.jsx
+++ front/src/components/Aleatorio/Aleatorio.jsx
@@ -1,34 +1,37 @@
-import React, { useContext } from "react";
-
-import { SWContext } from "../../context/context";
-
+import React, { useContext } from 'react';
+import { SWContext } from '../../context/context';
+import Cromo from '../Cromo/Cromo';
+import './Aleatorio.scss';
 
 const Aleatorio = () => {
   const { cromos } = useContext(SWContext);
 
   let cromosDiarios = Math.floor(Math.random() * (10 - 1) + 0);
 
-  let pinta = cromos[cromosDiarios].imagen;
-
+  let pinta = cromos[cromosDiarios];
 
   let cromosDiarios1 = Math.floor(Math.random() * (10 - 1) + 0);
 
-  let pinta1 = cromos[cromosDiarios1].rol;
+  let pinta1 = cromos[cromosDiarios1];
 
   let cromosDiarios2 = Math.floor(Math.random() * (10 - 1) + 0);
 
-  let pinta2 = cromos[cromosDiarios2].nombre;
-  
+  let pinta2 = cromos[cromosDiarios2];
 
   return (
-    <div>
-      
-      <h3> el cromo al azar es: {pinta}</h3>
-      <h3> el cromo al azar es: {pinta1}</h3>
-      <h3> el cromo al azar es: {pinta2}</h3>
-
-
-
+    <div className='aleatorio'>
+      <figure classname='figure'>
+        <Cromo cromoImg={pinta.imagen} cromoNombre={pinta.nombre}></Cromo>
+        <button className='añadir'>AÑADIR</button>
+      </figure>
+      <figure classname='figure'>
+        <Cromo cromoImg={pinta1.imagen} cromoNombre={pinta1.nombre}></Cromo>
+        <button className='añadir'>AÑADIR</button>
+      </figure>
+      <figure classname='figure'>
+        <Cromo cromoImg={pinta2.imagen} cromoNombre={pinta2.nombre}></Cromo>
+        <button className='añadir'>AÑADIR</button>
+      </figure>
     </div>
   );
 };
