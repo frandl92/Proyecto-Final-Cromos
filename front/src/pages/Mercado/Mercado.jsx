21f0de9cf61697122e04fab966988ee7c30ad4fa
Wida30 <wida30@yahoo.es>
2022-07-13 00:22:57 +0200

    boton mercado

--- front/src/pages/Mercado/Mercado.jsx
+++ front/src/pages/Mercado/Mercado.jsx
@@ -9,15 +9,36 @@ import Swal from "sweetalert2";
 const Mercado = () => {
   const [mercado, setMercado] = useState();
   const { user } = useContext(JwtContext);
+  const [algo, setAlgo] = useState();
+  const x = []; 
+  const [verdad, setVerdad] = useState(false)
+ console.log(user.album)
+  console.log(mercado)
+
+
+
+  const coñoya = ()=>{
+    for (const iterator of user.album) {
+      x.push(iterator._id)
+      setAlgo(x)
+      
+
+    }
+
+    
+  }
+   
 
   const getMercado = async () => {
     const mercadoAPI = await fetch("http://localhost:8005/mercado");
     const mercadoJSON = await mercadoAPI.json();
     setMercado(mercadoJSON.Mercado);
     console.log(mercadoJSON.Mercado);
+    coñoya();
   };
   useEffect(() => {
     getMercado();
+    
   }, []);
 
   const onClickk = (cromo1, user1, wanted, merId) => {
@@ -37,36 +58,42 @@ const Mercado = () => {
     });
   };
 
+  // const buscar = (quebusco) => {
+  //   for (const iterator of user.album) {
+  //     if (iterator.includes(quebusco)) {
+  //       setAlgo(true);
+  //     }
+  //   }
+  // };
+
   return (
     <div className="mercado">
       {mercado &&
         mercado.map((mer) => (
           <div key={mer._id} className="cambio">
+            <div className="doscartas">
+              <div className="cartaparacambiar">
+                <h2>{mer.user1Id.usuario} quiere vender a: </h2>
+                <Cromo
+                  cromoImg={mer.cromo1Id.imagen}
+                  cromoNombre={mer.cromo1Id.nombre}
+                />
+              </div>
 
-          <div className="doscartas">
-            <div className="cartaparacambiar">
-              <h2>{mer.user1Id.usuario} quiere vender a: </h2>
-              <Cromo
-                cromoImg={mer.cromo1Id.imagen}
-                cromoNombre={mer.cromo1Id.nombre}
-              />
+              <img src="./assets/flecha2.gif" alt="flecha" />
+
+              <div className="cartaquiero">
+                <h2>por </h2>
+
+                <Cromo
+                  cromoImg={mer.wanted.imagen}
+                  cromoNombre={mer.wanted.nombre}
+                />
+              </div>
             </div>
 
-            {/* <img src="./assets/flecha.gif" alt="flecha"/> */}
-            <img src="./assets/flecha2.gif" alt="flecha"/>
-
-
-
-            <div className="cartaquiero">
-              <h2>por </h2>
-
-              <Cromo
-                cromoImg={mer.wanted.imagen}
-                cromoNombre={mer.wanted.nombre}
-              />
-            </div>
-
-            </div>
+            {algo.includes(mer.wanted._id) && <button onClick={()=> onClickk(mer.cromo1Id._id, mer.user1Id._id, mer.wanted._id, mer._id)}> ACEPTAR CAMBIO</button>}
+{/* 
             <button
               onClick={() =>
                 onClickk(
@@ -75,13 +102,16 @@ const Mercado = () => {
                   mer.wanted._id,
                   mer._id
                 )
-              } className="linkregistro"
+              }
+              className="linkregistro"
             >
               {" "}
               ACEPTAR CAMBIO
-            </button>
+            </button> */}
 
-            {/* {user.album.includes(mer.cromo1Id) ? <button onClick={()=> onClickk(mer.cromo1Id._id, mer.user1Id._id, mer.wanted._id, mer._id)}> ACEPTAR CAMBIO</button> : null } */}
+            {/* {verdad  ?
+            
+             <button onClick={()=> onClickk(mer.cromo1Id._id, mer.user1Id._id, mer.wanted._id, mer._id)}> ACEPTAR CAMBIO</button> : null } */}
           </div>
         ))}
     </div>
